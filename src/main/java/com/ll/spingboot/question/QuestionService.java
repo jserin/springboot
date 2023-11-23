f3af8eefceb4ca6cf9ba74e70a17074ef260904a
Jserin <jsr3643@naver.com>
2023-11-24 01:36:49 +0900

    231123 검색

--- src/main/java/com/ll/spingboot/question/QuestionService.java
+++ src/main/java/com/ll/spingboot/question/QuestionService.java
@@ -1,12 +1,15 @@
 package com.ll.spingboot.question;
 
 import com.ll.spingboot.DataNotFoundException;
+import com.ll.spingboot.answer.Answer;
 import com.ll.spingboot.user.SiteUser;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -45,11 +48,12 @@ public class QuestionService {
         return q;
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page,15, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
 
     public void modify(Question question, String subject, String content) {
